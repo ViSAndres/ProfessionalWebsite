afa503c284faad46a7a17a30bbfd5dd94c990d56
Carlos Andres Neira <carlosneira1997@gmail.com>
2021-09-09 20:48:57 -0400

    Initial commit

---
+++ src/Components/Body/Body.js
@@ -0,0 +1,9 @@
+import MeLol from '../../Media/meLol.jpg';
+import './Body.css';
+
+function Body() {
+    return(
+        <img src={MeLol} className="picture-me" alt="me"></img>
+    )
+}
+export default Body;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import './Header.css';
+
+function Header() {
+    return(
+        <div className ='Header'>
+                Carlos Andres Neira
+        </div>
+    );
+}
+export default Header;
